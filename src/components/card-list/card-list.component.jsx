bdfab3c7459c8ff682ef32970d16637f3f9316a3
gaylonalfano <gaylon.alfano@gmail.com>
2019-09-08 20:53:35 +0800

    Added first CardList component using CSS Grid. Working with props. Created a components folder with a card-list subfolder. Next going to create the Card component to hold each card. The CardList component is more like a container for our Card components that we'll reuse.

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,8 @@
+// components/card-list
+import React from 'react';
+
+import './card-list.styles.css';
+
+export const CardList = (props) => {
+  return <div className='card-list'>{props.children}</div>
+}
