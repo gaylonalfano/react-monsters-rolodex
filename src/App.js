d60af3985f12791209991c782e517cbc68d1f527
gaylonalfano <gaylon.alfano@gmail.com>
2019-09-13 14:52:56 +0800

    Finished the mini project. Covered state and the this keyword context and how arrow functions used as class methods are lexically scoped by JS. This automatically sets the this context to wherever the arrow function was first defined. Going to deploy to GH pages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -19,9 +25,16 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
