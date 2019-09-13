cc682af6367d1636e9ea2f1f4dad8b875d957489
gaylonalfano <gaylon.alfano@gmail.com>
2019-09-13 15:07:12 +0800

    Updates

---
+++ precache-manifest.dfcf33547e65f1956038f93d5c94209f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd4e74204ad3cf075fa7c68eb2f1394",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7ede89be4abf05d21ba",
+    "url": "/react-monsters-rolodex/static/css/main.a88cb654.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/react-monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "e7ede89be4abf05d21ba",
+    "url": "/react-monsters-rolodex/static/js/main.ea98007d.chunk.js"
+  },
+  {
+    "revision": "52565b9c45395d2918ea",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.dee6fb62.js"
+  }
+]);
